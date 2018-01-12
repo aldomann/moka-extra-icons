460c1c21f8798eff2786bf48880179daf99335bc
Alfredo Hernández <aldomann.designs@gmail.com>
2018-01-12 22:16:12 +0100

    Added telegram-desktop symlink

---
+++ symlinks.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/16x16/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/16x16@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/22x22/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/22x22@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/24x24/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/24x24@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/32x32/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/32x32@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/48x48/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/48x48@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/64x64/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/64x64@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/96x96/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/96x96@2x/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/256x256/web/;
+ln -s telegram.png telegram-desktop.png;
+
+cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/256x256@2x/web/;
+ln -s telegram.png telegram-desktop.png;
