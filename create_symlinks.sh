fc17d96d72d4d403329f4a064e188d40b3b4c73c
Alfredo Hernández <aldomann.designs@gmail.com>
2018-10-03 03:56:57 +0200

    Added symlinks for GNOME Power Statistics and Marker

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -13,20 +13,23 @@ function SymLink() {
 # Loop through Apps
 for size in "${dirs[@]}"; do
 	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/apps/;
-	SymLink "dconf-editor.png"       "ca.desrt.dconf-editor.png";
-	SymLink "dconf-editor.png"       "ca.desrt.dconf-editor.png";
-	SymLink "gnome-tweak-tool.png"   "org.gnome.tweaks.png";
-	SymLink "discord.png"            "com.discordapp.Discord.png";
-	SymLink "webtorrent-desktop.png" "io.webtorrent.WebTorrent.png";
-	SymLink "skype.png"              "com.skype.Client.png";
-	SymLink "gpmdp.png"              "com.googleplaymusicdesktopplayer.GPMDP.png";
-	SymLink "gimp.png"               "org.gimp.GIMP.png";
-	SymLink "gpick.png"              "nl.hjdskes.gcolor3.png";
-	SymLink "aoe2-hd.png"            "steam_icon_221380.png";
-	SymLink "civ4.png"               "steam_icon_8800.png";
-	SymLink "onlyoffice.png"         "asc-de.png";
-	SymLink "hamster.png"            "hamster-time-tracker.png";
-	SymLink "pdfshuffler.png"        "pdftag.png";
+	SymLink "dconf-editor.png"            "ca.desrt.dconf-editor.png";
+	SymLink "dconf-editor.png"            "ca.desrt.dconf-editor.png";
+	SymLink "gnome-tweak-tool.png"        "org.gnome.tweaks.png";
+	SymLink "discord.png"                 "com.discordapp.Discord.png";
+	SymLink "webtorrent-desktop.png"      "io.webtorrent.WebTorrent.png";
+	SymLink "skype.png"                   "com.skype.Client.png";
+	SymLink "gpmdp.png"                   "com.googleplaymusicdesktopplayer.GPMDP.png";
+	SymLink "gimp.png"                    "org.gimp.GIMP.png";
+	SymLink "gpick.png"                   "nl.hjdskes.gcolor3.png";
+	SymLink "aoe2-hd.png"                 "steam_icon_221380.png";
+	SymLink "civ4.png"                    "steam_icon_8800.png";
+	SymLink "onlyoffice.png"              "asc-de.png";
+	SymLink "hamster.png"                 "hamster-time-tracker.png";
+	SymLink "pdfshuffler.png"             "pdftag.png";
+	SymLink "gnome-power-statistics.png"  "org.gnome.PowerStats.png";
+	SymLink "accessories-text-editor.png" "com.github.fabiocolacio.marker.png";
+
 done
 
 # Loop through Web Apps
