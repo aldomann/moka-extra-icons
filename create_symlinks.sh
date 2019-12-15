0f3011702537452ea3a6d9bf5608dc7b69061e5a
Alfredo Hernández <aldomann.designs@gmail.com>
2019-12-15 00:24:38 +0000

    Symlinks for Spotify, Authy, Dolphin Emu

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -16,41 +16,41 @@ function SymLink() {
 # Loop through Apps
 for size in "${dirs[@]}"; do
 	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/apps/;
+	SymLink "accessories-text-editor.png"          "com.github.fabiocolacio.marker.png";
 	SymLink "anydesk.png"                          "com.anydesk.Anydesk.png";
-	SymLink "discord.png"                          "com.discordapp.Discord.png";
-	SymLink "webtorrent-desktop.png"               "io.webtorrent.WebTorrent.png";
-	SymLink "skype.png"                            "com.skype.Client.png";
-	SymLink "gpmdp.png"                            "com.googleplaymusicdesktopplayer.GPMDP.png";
-	SymLink "gimp.png"                             "org.gimp.GIMP.png";
-	SymLink "gpick.png"                            "nl.hjdskes.gcolor3.png";
 	SymLink "aoe2-hd.png"                          "steam_icon_221380.png";
 	SymLink "civ4.png"                             "steam_icon_8800.png";
-	SymLink "onlyoffice.png"                       "asc-de.png";
-	SymLink "hamster.png"                          "hamster-time-tracker.png";
-	SymLink "pdfshuffler.png"                      "pdftag.png";
-	SymLink "accessories-text-editor.png"          "com.github.fabiocolacio.marker.png";
 	SymLink "com.github.needleandthread.vocal.png" "com.github.needle-and-thread.vocal.png";
+	SymLink "discord.png"                          "com.discordapp.Discord.png";
+	SymLink "dolphin-emu.png"                      "org.DolphinEmu.dolphin-emu.png";
+	SymLink "gimp.png"                             "org.gimp.GIMP.png";
+	SymLink "gpick.png"                            "nl.hjdskes.gcolor3.png";
+	SymLink "gpmdp.png"                            "com.googleplaymusicdesktopplayer.GPMDP.png";
+	SymLink "hamster.png"                          "hamster-time-tracker.png";
+	SymLink "onlyoffice.png"                       "asc-de.png";
 	SymLink "pdfmod.png"                           "masterpdfeditor5.png";
+	SymLink "pdfshuffler.png"                      "pdftag.png";
 	SymLink "popcorntime.png"                      "sh.popcorntime.PopcornTime.png"
+	SymLink "skype.png"                            "com.skype.Client.png";
+	SymLink "webtorrent-desktop.png"               "io.webtorrent.WebTorrent.png";
 done
 
 # Loop through GNOME Apps
 for size in "${dirs[@]}"; do
 	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/apps/;
-	SymLink "dconf-editor.png"                     "ca.desrt.dconf-editor.png";
-	SymLink "dconf-editor.png"                     "ca.desrt.dconf-editor.png";
-	SymLink "gnome-tweak-tool.png"                 "org.gnome.tweaks.png";
-	SymLink "gimp.png"                             "org.gimp.GIMP.png";
-	SymLink "gnome-power-statistics.png"           "org.gnome.PowerStats.png";
 	SymLink "applets-screenshooter.png"            "org.gnome.Screenshot.png";
 	SymLink "archive-manager.png"                  "org.gnome.ArchiveManager.png";
+	SymLink "dconf-editor.png"                     "ca.desrt.dconf-editor.png";
 	SymLink "disk-usage-analyzer.png"              "org.gnome.baobab.png";
+	SymLink "gimp.png"                             "org.gimp.GIMP.png";
 	SymLink "gnome-disks.png"                      "org.gnome.DiskUtility.png";
+	SymLink "gnome-power-statistics.png"           "org.gnome.PowerStats.png";
+	SymLink "gnome-tweak-tool.png"                 "org.gnome.tweaks.png";
 	SymLink "logviewer.png"                        "org.gnome.Logs.png";
-	SymLink "preferences-desktop-font.png"         "org.gnome.font-viewer.png";
-	SymLink "preferences-system.png"               "org.gnome.Settings.png";
 	SymLink "org.gnome.SystemMonitor.png"          "gnome-system-monitor.png";
 	SymLink "org.gnome.SystemMonitor.png"          "org.gnome.Monitor.png";
+	SymLink "preferences-desktop-font.png"         "org.gnome.font-viewer.png";
+	SymLink "preferences-system.png"               "org.gnome.Settings.png";
 	SymLink "utilities-terminal.png"               "org.gnome.Terminal.png";
 done
 
@@ -58,5 +58,8 @@ done
 for size in "${dirs[@]}"; do
 	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/web/;
 	SymLink "telegram.png" "telegram-desktop.png";
+	SymLink "chrome-gaedmjdfmmahhbjefcbgaolhhanlaolb-Default.png" "authy.png";
+	SymLink "web-spotify.png"                                     "com.spotify.Client.png";
+
 done
 
