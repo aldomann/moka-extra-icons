aac9e814c814931a3ee00715d8d52cc72c39302a
Alfredo Hernández <aldomann.designs@gmail.com>
2019-12-22 01:39:56 +0000

    Added symlinks for io.otsaloma.gaupol, org.gnome.SimpleScan, org.gnome.SoundRecorder, org.gnome.Sysprof, TeamViewer, and whatsapp-nativefier

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# moka-extra-icons directory
+iconsDir=/home/aldomann/Git/snwh/moka-extra-icons/Moka
+
 # List of icon sizes
 declare -a dirs=("16x16" "16x16@2x" "22x22" "22x22@2x" "24x24" "24x24@2x" "32x32" "32x32@2x" "48x48" "48x48@2x" "64x64" "64x64@2x" "96x96" "96x96@2x" "256x256" "256x256@2x")
 
@@ -15,7 +18,7 @@ function SymLink() {
 
 # Loop through Apps
 for size in "${dirs[@]}"; do
-	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/apps/;
+	cd $iconsDir/$size/apps/;
 	SymLink "accessories-text-editor.png"          "com.github.fabiocolacio.marker.png";
 	SymLink "anydesk.png"                          "com.anydesk.Anydesk.png";
 	SymLink "aoe2-hd.png"                          "steam_icon_221380.png";
@@ -33,11 +36,13 @@ for size in "${dirs[@]}"; do
 	SymLink "popcorntime.png"                      "sh.popcorntime.PopcornTime.png"
 	SymLink "skype.png"                            "com.skype.Client.png";
 	SymLink "webtorrent-desktop.png"               "io.webtorrent.WebTorrent.png";
+	SymLink "gaupol.png"                           "io.otsaloma.gaupol.png"
+	SymLink "teamviewer.png"                       "TeamViewer.png"
 done
 
 # Loop through GNOME Apps
 for size in "${dirs[@]}"; do
-	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/apps/;
+	cd $iconsDir/$size/apps/;
 	SymLink "applets-screenshooter.png"            "org.gnome.Screenshot.png";
 	SymLink "archive-manager.png"                  "org.gnome.ArchiveManager.png";
 	SymLink "dconf-editor.png"                     "ca.desrt.dconf-editor.png";
@@ -52,14 +57,17 @@ for size in "${dirs[@]}"; do
 	SymLink "preferences-desktop-font.png"         "org.gnome.font-viewer.png";
 	SymLink "preferences-system.png"               "org.gnome.Settings.png";
 	SymLink "utilities-terminal.png"               "org.gnome.Terminal.png";
+	SymLink "gnome-sound-recorder.png"             "org.gnome.SoundRecorder.png";
+	SymLink "simple-scan.png"                      "org.gnome.SimpleScan.png";
+	SymLink "sysprof.png"                          "org.gnome.Sysprof.png";
 done
 
 # Loop through Web Apps
 for size in "${dirs[@]}"; do
-	cd /home/aldomann/Git/snwh/moka-extra-icons/Moka/$size/web/;
-	SymLink "telegram.png" "telegram-desktop.png";
+	cd $iconsDir/$size/web/;
+	SymLink "telegram.png"                                        "telegram-desktop.png";
 	SymLink "chrome-gaedmjdfmmahhbjefcbgaolhhanlaolb-Default.png" "authy.png";
 	SymLink "web-spotify.png"                                     "com.spotify.Client.png";
-
+	SymLink "whatsapp.png"                                        "whatsapp-nativefier.png";
 done
 
