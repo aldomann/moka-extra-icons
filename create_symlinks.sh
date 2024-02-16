53d2196b930c099a498663cdb7ecc26862be6d3c
Alfredo Hernández <aldomann.designs@gmail.com>
2024-02-16 12:52:16 +0000

    Added new symlinks

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # moka-extra-icons directory
-iconsDir=/home/aldomann/Git/snwh/moka-extra-icons/Moka
+iconsDir=/home/aldomann/Git/icons/moka-extra-icons/Moka
 
 # List of icon sizes
 declare -a dirs=("16x16" "16x16@2x" "22x22" "22x22@2x" "24x24" "24x24@2x" "32x32" "32x32@2x" "48x48" "48x48@2x" "64x64" "64x64@2x" "96x96" "96x96@2x" "256x256" "256x256@2x")
@@ -22,22 +22,30 @@ for size in "${dirs[@]}"; do
 	SymLink "accessories-text-editor.png"          "com.github.fabiocolacio.marker.png";
 	SymLink "anydesk.png"                          "com.anydesk.Anydesk.png";
 	SymLink "aoe2-hd.png"                          "steam_icon_221380.png";
+	SymLink "calibre.png"                          "com.calibre_ebook.calibre.png";
+	SymLink "chromium-browser.png"                 "org.chromium.Chromium.png";
 	SymLink "civ4.png"                             "steam_icon_8800.png";
 	SymLink "com.github.needleandthread.vocal.png" "com.github.needle-and-thread.vocal.png";
 	SymLink "discord.png"                          "com.discordapp.Discord.png";
 	SymLink "dolphin-emu.png"                      "org.DolphinEmu.dolphin-emu.png";
+	SymLink "gaupol.png"                           "io.otsaloma.gaupol.png"
 	SymLink "gimp.png"                             "org.gimp.GIMP.png";
 	SymLink "gpick.png"                            "nl.hjdskes.gcolor3.png";
 	SymLink "gpmdp.png"                            "com.googleplaymusicdesktopplayer.GPMDP.png";
 	SymLink "hamster.png"                          "hamster-time-tracker.png";
+	SymLink "inkscape.png"                         "org.inkscape.Inkscape.png"
 	SymLink "onlyoffice.png"                       "asc-de.png";
 	SymLink "pdfmod.png"                           "masterpdfeditor5.png";
 	SymLink "pdfshuffler.png"                      "pdftag.png";
 	SymLink "popcorntime.png"                      "sh.popcorntime.PopcornTime.png"
 	SymLink "skype.png"                            "com.skype.Client.png";
-	SymLink "webtorrent-desktop.png"               "io.webtorrent.WebTorrent.png";
-	SymLink "gaupol.png"                           "io.otsaloma.gaupol.png"
+	SymLink "sigil.png"                            "com.sigil_ebook.Sigil.png";
 	SymLink "teamviewer.png"                       "TeamViewer.png"
+	SymLink "webtorrent-desktop.png"               "io.webtorrent.WebTorrent.png";
+	SymLink "hexchat.png"                          "io.github.Hexchat.png";
+	SymLink "enpass6.png"                          "io.enpass.Enpass.png";
+	SymLink "yacreader.png"                        "com.yacreader.YACReader.png";
+	SymLink "yacreader-library.png"                "com.yacreader.YACReader.YACReaderLibrary.png";
 done
 
 # Loop through GNOME Apps
@@ -62,6 +70,15 @@ for size in "${dirs[@]}"; do
 	SymLink "sysprof.png"                          "org.gnome.Sysprof.png";
 done
 
+# Loop through Games
+for size in "${dirs[@]}"; do
+	cd $iconsDir/$size/apps/;
+	SymLink "aoe2-hd.png"                          "steam_icon_221380.png";
+	SymLink "civ4.png"                             "steam_icon_8800.png";
+	SymLink "civ6.png"                             "steam_icon_289070.png";
+	SymLink "dolphin-emu.png"                      "org.DolphinEmu.dolphin-emu.png";
+done
+
 # Loop through Web Apps
 for size in "${dirs[@]}"; do
 	cd $iconsDir/$size/web/;
@@ -70,4 +87,3 @@ for size in "${dirs[@]}"; do
 	SymLink "web-spotify.png"                                     "com.spotify.Client.png";
 	SymLink "whatsapp.png"                                        "whatsapp-nativefier.png";
 done
-
